d1d3b2d83d8f8f37b22631db8d4d7eab3a89e1eb
Shivam Mishra <scm.mymail@gmail.com>
2021-09-01 12:54:48 +0530

    feat: add packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,27 @@
-const shiki = require('shiki')
+const shiki = require("shiki");
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Shivam Mishra',
+    title: "Shivam Mishra",
     htmlAttrs: {
-      lang: 'en',
+      lang: "en",
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
-    script: [{ src: 'https://cdn.splitbee.io/sb.js', async: true, crossorigin: "anonymous" }]
+    meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }, { hid: "description", name: "description", content: "" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+    script: [{ src: "https://cdn.splitbee.io/sb.js", async: true, crossorigin: "anonymous" }],
   },
 
-//   fontLoader: {
-//     url:
-//       'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
+  //   fontLoader: {
+  //     url:
+  //       'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
 
-//     prefetch: true,
-//     preconnect: true,
-//   },
+  //     prefetch: true,
+  //     preconnect: true,
+  //   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -39,19 +35,20 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "nuxt-vite",
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-    '@nuxtjs/robots'
+    "@nuxt/content",
+    "@nuxtjs/robots",
   ],
 
   robots: {
-    UserAgent: '*',
-    Disallow: ['/cya-soon', '/thank-you'],
+    UserAgent: "*",
+    Disallow: ["/cya-soon", "/thank-you"],
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
@@ -60,14 +57,14 @@ export default {
       async highlighter() {
         const highlighter = await shiki.getHighlighter({
           // Complete themes: https://github.com/shikijs/shiki/tree/master/packages/themes
-          theme: 'min-dark'
-        })
+          theme: "min-dark",
+        });
 
         return (rawCode, lang) => {
-          return highlighter.codeToHtml(rawCode, lang)
-        }
-      }
-    }
+          return highlighter.codeToHtml(rawCode, lang);
+        };
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -76,4 +73,4 @@ export default {
     cache: true,
     hardsource: true,
   },
-}
+};
